02041b1083d214e8c5a706c04f61b6b6bbbc5fe2
Prasino <prasino.fu@gmail.com>
2020-10-04 20:06:46 +0800

    pg pool

--- app-generated/routes/pg.js
+++ app-generated/routes/pg.js
@@ -6,20 +6,61 @@ router.get('/', function (ctx, next) {
   ctx.body = 'this is a pg response!'
 })
 
-const { Client } = require('pg');
-const client = new Client({
+const { Client, Pool } = require('pg');
+const pool = new Pool({
     user: 'koa_admin1',
     host: 'localhost',
     database: 'koa_admin1',
     password: 'XCODER4koa1!',
     port: 5432,
 });
-
-client.connect();
-
-client.query('SELECT * FROM users', (err, res) => {
-    console.log('connected', res);
-    console.log(err ? err.stack : res.rows[0].name.length) // Hello World!
-    client.end()
+pool.on('error', (err, client) => {
+    console.error('Unexpected error on idle client', err)
+    process.exit(-1)
 })
+
+
+// callback - checkout a client
+pool.connect((err, client, done) => {
+  if (err) throw err
+  client.query('SELECT * FROM users', (err, res) => {
+    done()
+    if (err) {
+      console.log(err.stack)
+    } else {
+      console.log('row 001', res.rows[0])
+    }
+  })
+})
+
+pool.connect()
+  .then(client => {
+    return client
+    .query('SELECT * FROM users')
+    .then(res => {
+      // client.release();
+      console.log('connected 2', res);
+    })
+    .catch(err => {
+      client.release()
+      console.log(err.stack)
+    })
+  })
+  .catch(err => {
+    console.log(err.stack)
+  });
+
+  // async/await - check out a client
+;(async () => {
+  const client = await pool.connect()
+  try {
+    const res = await client.query('SELECT * FROM users WHERE id = $1', [1])
+    console.log('row 0', res.rows[0])
+  } finally {
+    // Make sure to release the client before any error handling,
+    // just in case the error handling itself throws an error.
+    client.release()
+  }
+})().catch(err => console.log(err.stack))
+
 module.exports = router
